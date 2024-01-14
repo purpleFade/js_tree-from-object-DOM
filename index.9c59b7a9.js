36a9a1ce8fe7919f84ed8d50049fcae6d8e87543
purplefade <tarovykanton@gmail.com>
2024-01-14 11:35:17 +0200

    make build

---
+++ index.9c59b7a9.js
@@ -0,0 +1,2 @@
+!function e(t,n){let l=document.createElement("ul");for(let r in t.appendChild(l),n){let t=document.createElement("li");t.textContent=r,Object.keys(n[r]).length>0&&e(t,n[r]),l.appendChild(t)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.9c59b7a9.js.map
